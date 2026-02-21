6b4c5b0c8fb16d639291ec4e07b72cde244c615e
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-21 16:45:08 +0500

    Update ElytraFlightMixin.java

--- src/main/java/name/modid/mixin/ElytraFlightMixin.java
+++ src/main/java/name/modid/mixin/ElytraFlightMixin.java
@@ -8,22 +8,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(PlayerEntity.class)
 public abstract class ElytraFlightMixin {
-    
     @Inject(method = "tick", at = @At("TAIL"))
     private void onTick(CallbackInfo ci) {
         PlayerEntity player = (PlayerEntity) (Object) this;
-        
-        // Работает только на клиенте, чтобы не было десинхрона
         if (player.getWorld().isClient) {
             if (player.isFallFlying()) {
-                // Разрешаем полет
                 player.getAbilities().flying = true;
-                
-                // Устанавливаем стабильную скорость
                 player.getAbilities().setFlySpeed(0.05f);
             }
         }
     }
 }
+
 
 
