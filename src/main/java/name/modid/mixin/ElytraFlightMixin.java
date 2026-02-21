9e66f5ddbdcc61e89af0d0326d90f494d0999462
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-21 15:47:47 +0500

    Update ElytraFlightMixin.java

--- src/main/java/name/modid/mixin/ElytraFlightMixin.java
+++ src/main/java/name/modid/mixin/ElytraFlightMixin.java
@@ -1,22 +1,22 @@
 package name.modid.mixin;
 
-import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.network.protocol.game.ServerboundPlayerCommandPacket;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(LocalPlayer.class)
-public class ElytraFlightMixin {
+@Mixin(Entity.class)
+public class EntityMixin {
     @Inject(method = "tick", at = @At("HEAD"))
     private void onTick(CallbackInfo ci) {
-        LocalPlayer player = (LocalPlayer) (Object) this;
-        if (player.isFallFlying()) {
-            // Летим вперед туда, куда смотрим
-            player.setDeltaMovement(player.getDeltaMovement().add(player.getLookAngle().scale(0.08)));
-            // Шлем пакет серверу, чтобы не выбило
-            player.connection.send(new ServerboundPlayerCommandPacket(player, ServerboundPlayerCommandPacket.Action.START_FALL_FLYING));
+        if ((Object) this instanceof PlayerEntity player) {
+            if (player.isFallFlying()) {
+                // Логика полета: убираем падение и добавляем скорость вперед
+                player.getAbilities().flying = true;
+                player.getAbilities().setFlySpeed(0.05f);
+            }
         }
     }
 }
