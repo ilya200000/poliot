fc341cd1cf0b000062f256babb8b7b375b60dc75
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-22 21:44:14 +0500

    Update ElytraFlightMixin.java

--- src/main/java/name/modid/mixin/ElytraFlightMixin.java
+++ src/main/java/name/modid/mixin/ElytraFlightMixin.java
@@ -1,5 +1,6 @@
 package name.modid.mixin;
 
+import name.modid.ElytraData;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket;
 import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
@@ -24,15 +25,14 @@ public abstract class ElytraFlightMixin {
         if (player.isFallFlying()) {
             player.setVelocity(0, 0, 0);
 
-            double speed = 0.18; // Сейвовая скорость
+            double speed = 0.18; 
             Vec3d look = player.getRotationVec(1.0F);
             
             double x = player.getX() + (player.input.pressingForward ? look.x * speed : 0);
             double y = player.getY() + (player.input.jumping ? 0.12 : (player.input.sneaking ? -0.2 : -0.002));
             double z = player.getZ() + (player.input.pressingForward ? look.z * speed : 0);
 
-            // Используем метод из NetworkMixin
-            if (NetworkMixin.getLastId() != -1) {
+            if (ElytraData.lastTeleportId != -1) {
                 player.networkHandler.sendPacket(new PlayerMoveC2SPacket.Full(x, y, z, player.getYaw(), player.getPitch(), false));
             }
             
