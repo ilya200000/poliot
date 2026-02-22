c4f68f8d3acec2393513f3733968b56442280511
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-22 21:25:34 +0500

    Update NetworkMixin.java

--- src/main/java/name/modid/mixin/NetworkMixin.java
+++ src/main/java/name/modid/mixin/NetworkMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.network.packet.c2s.play.TeleportConfirmC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,16 +12,22 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ClientPlayNetworkHandler.class)
 public class NetworkMixin {
     
-    public static int lastTeleportId = -1;
+    @Unique
+    private static int lastTeleportId = -1;
 
     @Inject(method = "onPlayerPositionLook", at = @At("HEAD"))
     private void onPlayerPositionLook(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
         lastTeleportId = packet.getTeleportId();
         
-        // Используем более безопасный способ отправки пакета
         ClientPlayNetworkHandler handler = (ClientPlayNetworkHandler) (Object) this;
         if (lastTeleportId != -1) {
             handler.sendPacket(new TeleportConfirmC2SPacket(lastTeleportId));
         }
     }
+
+    // Метод, чтобы другой миксин мог узнать ID
+    public static int getLastId() {
+        return lastTeleportId;
+    }
 }
+
