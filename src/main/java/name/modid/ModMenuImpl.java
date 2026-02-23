0004826b0f1a57f87273b0a7f5341af1afc93401
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-23 12:38:13 +0500

    Update ModMenuImpl.java

--- src/main/java/name/modid/ModMenuImpl.java
+++ src/main/java/name/modid/ModMenuImpl.java
@@ -3,13 +3,16 @@ package name.modid;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 
 public class ModMenuImpl implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        // Инициализация конфига при первом запуске
-        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        // Регистрируем конфиг (оборачиваем в try-catch на всякий случай)
+        try {
+            AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        } catch (Exception ignored) {}
+        
         return parent -> AutoConfig.getConfigScreen(ModConfig.class, parent).get();
     }
 }
