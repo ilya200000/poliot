5b4906b32ad3e7f7236419fd5750b1db5ddac412
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-23 12:29:39 +0500

    Update ModConfig.java

--- src/main/java/name/modid/ModConfig.java
+++ src/main/java/name/modid/ModConfig.java
@@ -1,1 +1,15 @@
+package name.modid;
+
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+
+@Config(name = "poliot")
+public class ModConfig implements ConfigData {
+    public boolean totemEnabled = true;
+    public int totemDelay = 50; // Задержка в мс (для Grim)
+
+    public boolean autoBuyEnabled = false;
+    public int shopSlot = 20; // Слот в магазине
+    public String shopTitle = "Магазин";
+}
 
