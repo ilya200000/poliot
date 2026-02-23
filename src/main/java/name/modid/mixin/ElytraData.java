a743d9414187101da142d6809487c7be46d1e689
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-23 11:07:47 +0500

    Update ElytraData.java

--- src/main/java/name/modid/mixin/ElytraData.java
+++ src/main/java/name/modid/mixin/ElytraData.java
@@ -1,8 +1,13 @@
 package name.modid;
 
+import net.minecraft.network.packet.Packet;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ElytraData {
+    public static final LinkedBlockingQueue<Packet<?>> packetQueue = new LinkedBlockingQueue<>();
     public static int lastTeleportId = -1;
-    public static int tickCounter = 0;
+    public static boolean isFlying = false;
 }
+
 
 
