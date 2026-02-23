d6ec88b78d79c385c9d1d4edc72ccfdb76f95cc8
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-02-23 11:08:19 +0500

    Update NetworkMixin.java

--- src/main/java/name/modid/mixin/NetworkMixin.java
+++ src/main/java/name/modid/mixin/NetworkMixin.java
@@ -2,6 +2,8 @@ package name.modid.mixin;
 
 import name.modid.ElytraData;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import net.minecraft.network.packet.c2s.play.TeleportConfirmC2SPacket;
 import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
@@ -10,17 +12,21 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
-public class NetworkMixin {
+public abstract class NetworkMixin {
 
-    @Inject(method = "onPlayerPositionLook", at = @At("HEAD"))
-    private void onPlayerPositionLook(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
-        ElytraData.lastTeleportId = packet.getTeleportId();
-        
-        ClientPlayNetworkHandler handler = (ClientPlayNetworkHandler) (Object) this;
-        if (ElytraData.lastTeleportId != -1) {
-            handler.sendPacket(new TeleportConfirmC2SPacket(ElytraData.lastTeleportId));
+    @Inject(method = "sendPacket", at = @At("HEAD"), cancellable = true)
+    private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
+        if (ElytraData.isFlying && packet instanceof PlayerMoveC2SPacket) {
+            // Складываем пакеты движения в очередь вместо отправки
+            ElytraData.packetQueue.add(packet);
+            ci.cancel();
         }
     }
+
+    @Inject(method = "onPlayerPositionLook", at = @At("HEAD"))
+    private void onTeleport(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
+        ElytraData.lastTeleportId = packet.getTeleportId();
+        ClientPlayNetworkHandler handler = (ClientPlayNetworkHandler) (Object) this;
+        handler.sendPacket(new TeleportConfirmC2SPacket(ElytraData.lastTeleportId));
+    }
 }
-
-
