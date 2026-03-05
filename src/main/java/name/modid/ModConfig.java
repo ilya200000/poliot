00e3740440675ce90360eb58072274750a0eb92c
ilya200000 <145029327+ilya200000@users.noreply.github.com>
2026-03-05 20:42:43 +0500

    Update ModConfig.java

--- src/main/java/name/modid/ModConfig.java
+++ src/main/java/name/modid/ModConfig.java
@@ -1,14 +1,1 @@
-package name.modid;
 
-import me.shedaniel.autoconfig.ConfigData;
-import me.shedaniel.autoconfig.annotation.Config;
-
-@Config(name = "poliot")
-public class ModConfig implements ConfigData {
-    public boolean totemEnabled = true;
-    public int totemDelay = 60; 
-
-    public boolean shopEnabled = false;
-    public int shopSlot = 20;
-    public String shopTitle = "Магазин";
-}
